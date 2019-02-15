e91a2a6f8f8c00866de5f4b19f9b353d65a1ab7c
Max <hktxt2011@gmailcom>
2019-02-15 09:03:17 +0800

    2/15

---
+++ Courses/12. Python for Everybody/3. Using Python to Access Web Data/1.py
@@ -0,0 +1,29 @@
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://py4e-data.dr-chuck.net/comments_145040.html'
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+sum = 0
+for tag in tags:
+    # Look at the parts of a tag
+    sum = sum + int(tag.contents[0])
+    
+print('count:{}'.format(len(tags)))
+print('sum:{}'.format(sum))
