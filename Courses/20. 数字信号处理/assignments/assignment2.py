2f3dcc1e5ef94ec9bcdfb36b5db69bedc7502735
Max <hktxt2011@gmailcom>
2019-03-21 09:34:55 +0800

    assignment

---
+++ Courses/20. 数字信号处理/assignments/assignment2.py
@@ -0,0 +1,26 @@
+
+from scipy import signal
+
+def prototype_filter():
+    """ ASSIGNMENT 2
+
+        Compute the prototype filter used in subband coding. The filter
+        is a 512-point lowpass FIR h[n] with bandwidth pi/64 and stopband
+        starting at pi/32
+
+        You should use the remez routine (signal.remez()). See
+        http://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.remez.html
+    """
+
+    # Your code goes here
+    import numpy as np
+
+    M = 512     # number of taps
+    Fs = np.pi  # sampling rate
+    FPass = np.pi/128    # passband edge
+    Fstop = np.pi/32     # stopband edge
+
+    h = signal.remez(M, [0, FPass/2, Fstop/2, Fs/2], [2, 0], fs=Fs)
+    return h
+
+
---
+++ Courses/20. 数字信号处理/assignments/assignment3.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def subband_filtering(x, h):
+    """ ASSIGNMENT 3
+
+        Write a routine to implement the efficient version of the subband filter
+        as specified by the MP3 standard
+
+        Arguments:
+        x:  a new 512-point data buffer, in time-reversed order [x[n],x[n-1],...,x[n-511]].
+        h:  The prototype filter of the filter bank you found in the previous assignment
+
+        Returns:
+        s: 32 new output samples
+    """
+
+    # Your code goes here
+    r = h * x
+    q = np.arange(64)    # range from 0 to 63
+    c = np.sum((-1)**np.arange(8)[:, np.newaxis] * r[q + 64*np.arange(8)[:, np.newaxis]], axis=0)
+    # Compute subband outputs
+    s = np.sum(np.cos(np.pi / 64. * (2 * np.arange(32)[:, np.newaxis] + 1) * (np.arange(q.shape[0]) - 16))*c, axis=1)
+    return s
